b922633b3daed18230124a20e479dcb068bf9054
Alex Sugak <aleksandr.sugak@gmail.com>
2018-10-24 15:29:05 +0300

    initial commit: api and test projects with basic test running

---
+++ api/Controllers/Info.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class InfoController : ControllerBase
+    {
+        // GET api/
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello from the api";
+        }
+    }
+}
---
+++ test/EndToEnd.cs
@@ -0,0 +1,27 @@
+using System;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Xunit;
+using System.Threading.Tasks;
+using api;
+
+namespace test
+{
+    public class EndToEnd : IClassFixture<WebApplicationFactory<api.Startup>>
+    {
+        private readonly WebApplicationFactory<api.Startup> _factory;
+
+        public EndToEnd(WebApplicationFactory<api.Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task api_root_returns_success()
+        {
+            var client = _factory.CreateClient();
+            var response = await client.GetAsync("/api");
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
